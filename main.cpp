08e03e091d7e449c08d32c159013f094796dd3b9
Tinydude153 <42678254+Tinydude153@users.noreply.github.com>
2023-09-26 22:35:12 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include <windows.h>
 #include <tchar.h>
 #include <processthreadsapi.h>
+#include <wingdi.h>
+#include <dwmapi.h>
+#include <windef.h>
 
 struct ProcHwnd {
     DWORD proc_id;
     HWND  hwnd;
 };
+
+RECT itRect;
+bool getWindowRect;
 
 char tBuffer[64];
 int tSize = 0;
@@ -17,7 +23,7 @@ BOOL CALLBACK enum_windows_proc(HWND m_hwnd, LPARAM lParam) {
 
     window = GetWindow(window, GW_HWNDNEXT);
 
-    if (IsWindowVisible(m_hwnd)) {
+    /*if (IsWindowVisible(m_hwnd)) {
 
         do {
 
@@ -27,11 +33,12 @@ BOOL CALLBACK enum_windows_proc(HWND m_hwnd, LPARAM lParam) {
 
         } while (m_hwnd = GetWindow(m_hwnd, GW_HWNDNEXT));
 
-    }
+    }*/
 
     GetWindowThreadProcessId(m_hwnd, &processId);
     if (processId == ((ProcHwnd*)lParam)->proc_id) {
         ((ProcHwnd*)lParam)->hwnd = m_hwnd;
+        std::cout << std::endl << "EQUAL";
         return FALSE;
     }
     return TRUE;
@@ -39,7 +46,16 @@ BOOL CALLBACK enum_windows_proc(HWND m_hwnd, LPARAM lParam) {
 
 int main() {
 
-    STARTUPINFOA si { sizeof(STARTUPINFOA) };
+    STARTUPINFOA si;
+    memset(&si, 0, sizeof(STARTUPINFOA));
+    si.cb = sizeof(STARTUPINFOA);
+    si.dwX = 0;
+    si.dwY = 0;
+    si.lpTitle = (char*)"Window Testing";
+    si.dwXSize = 1000;
+    si.dwYSize = 1000;
+    si.wShowWindow = SW_HIDE;
+    si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USEPOSITION | STARTF_USESIZE;
     PROCESS_INFORMATION pi;
 
     const char* path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
@@ -57,13 +73,96 @@ int main() {
     WaitForInputIdle(pi.hProcess, INFINITE);
     HWND windowH;
     ProcHwnd ph { pi.dwProcessId, windowH };
+    Sleep(5000);
     EnumWindows(enum_windows_proc, (LPARAM)&ph);
 
-    if (!MoveWindow(ph.hwnd, 0, 0, 500, 500, TRUE)) {
+    Sleep(500);
+
+    HWND pWindow = GetParent(ph.hwnd);
+    if (pWindow != NULL) {
+
+        ph.hwnd = pWindow;
+
+    }
+
+    SetProcessDPIAware();
+    Sleep(5000);
+    // Windows deals with screen borders to account for resizing, so this is for offsetting that to get
+    // accurate screen sizes
+    RECT rect, frame;
+    GetWindowRect(ph.hwnd, &rect);
+    DwmGetWindowAttribute(ph.hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, &frame, sizeof(RECT));
+
+    RECT border;
+    border.left = frame.left - rect.left;
+    border.top = frame.top - rect.top;
+    border.right = frame.right - rect.right;
+    border.bottom = frame.bottom - rect.bottom;
+
+    rect.left -= border.left;
+    rect.top -= border.top;
+    rect.right -= border.right;
+    rect.bottom -= border.bottom;
+
+    std::cout << std::endl << rect.left;
+    std::cout << std::endl << rect.top;
+    std::cout << std::endl << rect.right;
+    std::cout << std::endl << rect.bottom;
+
+    HDWP deferWindow = BeginDeferWindowPos(1);
+
+    deferWindow = DeferWindowPos(
+        deferWindow,
+        ph.hwnd,
+        NULL,
+        0,
+        0,
+        2000,
+        400,
+        SWP_NOZORDER
+    );
+
+    if (deferWindow != NULL) {
+
+        if (!EndDeferWindowPos(deferWindow)) {
+
+            printf("\nEndDeferWindowPos failed: %d\n", GetLastError());
+
+        }
+
+    }
+
+    Sleep(5000);
+
+    UpdateWindow(ph.hwnd);
+
+    /*if (!SetWindowPos(ph.hwnd, HWND_BOTTOM, 0, 0, 1000, 500, SWP_HIDEWINDOW)) {
+
+        printf("\nSetWindowPos failed: %d\n", GetLastError());
+
+    }
+
+    Sleep(5000);
+
+    UpdateWindow(ph.hwnd);
+
+    if (!ShowWindow(ph.hwnd, SW_SHOW)) {
+
+        printf("\nShowWindow failed: %d\n", GetLastError());
+
+    }
+
+    UpdateWindow(ph.hwnd);
+    WaitForSingleObject(ph.hwnd, INFINITE);
+
+    /*if (!MoveWindow(ph.hwnd, 0, 0, 800, 200, TRUE)) {
 
         printf("\nMoveWindow failed: %d\n", GetLastError());
 
-    } 
+    } */
+
+    PostMessageA(ph.hwnd, WM_CLOSE, 0, 0);  
+    DestroyWindow(ph.hwnd);
 
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
@@ -98,19 +197,36 @@ int main() {
     CloseHandle(shellexec.hProcess); */
 
     DISPLAY_DEVICEA dDevices;
+    memset(&dDevices, 0, sizeof(DISPLAY_DEVICEA));
     dDevices.cb = sizeof(DISPLAY_DEVICEA);
 
-    EnumDisplayDevices(NULL, 1, &dDevices, EDD_GET_DEVICE_INTERFACE_NAME);
+    EnumDisplayDevices(NULL, 0, &dDevices, EDD_GET_DEVICE_INTERFACE_NAME);
     std::cout << std::endl << dDevices.DeviceString;
+
+    DISPLAY_DEVICEA dDevices_;
+    memset(&dDevices_, 0, sizeof(DISPLAY_DEVICEA));
+    dDevices_.cb = sizeof(DISPLAY_DEVICEA);
+
+    EnumDisplayDevices((const char*)dDevices.DeviceName, 0, &dDevices_, 0);
+    std::cout << std::endl << dDevices_.DeviceName;
 
     DEVMODE devmode;
     devmode.dmSize = sizeof(DEVMODE);
     devmode.dmDriverExtra = 512;
     char* dName = dDevices.DeviceName;
+
+    BITMAP monitor;
+    memset(&monitor, 1, (sizeof(BITMAP)));
+    HDC deviceDC = CreateDC((const char*)dDevices_.DeviceName, (const char*)dDevices_.DeviceName, NULL, NULL);
+    HGDIOBJ bmp = GetCurrentObject(deviceDC, OBJ_BITMAP);
+    GetObject(bmp, sizeof(BITMAP), &monitor);
+    std::cout << std::endl << monitor.bmWidth;
+    std::cout << std::endl << monitor.bmHeight;
+    std::cout << std::endl << GetDeviceCaps(deviceDC, HORZRES) << std::endl << GetDeviceCaps(deviceDC, VERTRES);
+
     EnumDisplaySettingsA(dName, ENUM_CURRENT_SETTINGS, &devmode);
     std::cout << std::endl << devmode.dmPelsWidth;
     std::cout << std::endl << devmode.dmPelsHeight;
     std::cout << std::endl << dName;
 
 }
-
